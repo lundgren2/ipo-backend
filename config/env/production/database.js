a8e4e3044ef67b5bf85963c71e8dccebf9d83827
Tobias Lundgren <tobias@lundgren.design>
2021-01-04 21:33:40 +0100

    Add docker + CI

---
+++ config/env/production/database.js
@@ -0,0 +1,26 @@
+module.exports = ({env}) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+        pool: {
+          acquireTimeoutMillis: 10000,
+          createTimeoutMillis: 10000,
+        },
+      },
+    },
+  },
+});
