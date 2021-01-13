9c6649f42f244929ff8c4e5a3996f880ced187f4
Tobias Lundgren <tobias@lundgren.design>
2021-01-13 21:07:48 +0100

    Use SSL in production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,16 +10,9 @@ module.exports = ({env}) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
       },
       options: {
-        ssl: env.bool('DATABASE_SSL', false),
-        pool: {
-          acquireTimeoutMillis: 10000,
-          createTimeoutMillis: 10000,
-        },
+        ssl: true,
       },
     },
   },
