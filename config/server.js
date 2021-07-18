6dc7c0b86d78cea5d3321fdde15c3056ddca8393
Tobias Lundgren <tobias@lundgren.design>
2021-07-19 00:41:06 +0200

    Disable cron jobs

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  cron: {
-    enabled: process.env.NODE_ENV === 'production' ? true : false,
-  },
+  // Disabled since we're using Kubernetes cron jobs
+  // cron: {
+  //   enabled: process.env.NODE_ENV === 'production' ? true : false,
+  // },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd64bf9b2e1cc4cb856db3ae2633c3074'),
