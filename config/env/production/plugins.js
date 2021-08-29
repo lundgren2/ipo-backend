7435416446cdf9ef71a3fe0369520f282f301c06
Tobias Lundgren <tobias@lundgren.design>
2021-08-29 22:13:57 +0200

    Add sentry to production

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,8 @@
 module.exports = ({env}) => ({
+  sentry: {
+    // Add Sentry DSN from: https://sentry.io/settings/ipo-se/projects/backend/keys/
+    dsn: 'https://f091ea00b30c4368b807caf475404bc0@o473428.ingest.sentry.io/5936188',
+  },
   upload: {
     provider: 'cloudinary',
     providerOptions: {
