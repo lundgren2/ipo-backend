357925b8dcb9f7c6143ce2f12f273015bb2d5bf3
Tobias Lundgren <tobias@lundgren.design>
2021-10-23 22:19:02 +0200

    Test new db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,19 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({env}) => {
-  const config = parse(env.DATABASE_URL || '');
-  if (!config) return {};
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false,
-          },
+module.exports = ({env}) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: {
+          ca: env('DATABASE_CA', ''),
         },
-        options: {},
       },
     },
-  };
-};
+  },
+});
