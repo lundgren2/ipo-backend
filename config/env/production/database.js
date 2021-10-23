d4a6f1b8aa5ab14980fa6b6ba2570970e04da378
Tobias Lundgren <tobias@lundgren.design>
2021-10-23 22:19:02 +0200

    Test new db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,15 +5,16 @@ module.exports = ({env}) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: {
-          ca: env('DATABASE_CA', ''),
+          rejectUnauthorized: false,
         },
       },
+      options: {},
     },
   },
 });
