94a4f5270c0c35dd0fb1fb914e3e029b6c86e935
Tobias Lundgren <tobias@lundgren.design>
2021-10-23 21:22:28 +0200

    Fix prod build

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,26 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({env}) => {
+  const config = parse(env.DATABASE_URL ?? '');
+  if (!config) return {};
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
+        options: {},
       },
-      options: {},
     },
-  },
-});
+  };
+};
